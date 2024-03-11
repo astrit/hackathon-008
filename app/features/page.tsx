decd447cfb92773baa4947446f890d5d6533a056
astrit <am@astrit.co>
2024-03-11 10:20:14 +0100

    Msg → Mon Mar 11 10:20:14 CET 2024

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,12 +1,14 @@
+import Content from "&/content/content"
+
 export default function Features() {
   return (
-    <div>
+    <Content>
       <h1>Features</h1>
       <ul>
         <li>Ask AI</li>
         <li>Accessability Checker</li>
         <li>Navigation: Audio & Motion</li>
       </ul>
-    </div>
+    </Content>
   )
 }
